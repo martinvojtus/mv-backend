80fd630256e3c002d7f900b10222f30e1789cdfd
martinvojtus <84767857+martinvojtus@users.noreply.github.com>
2026-03-15 14:06:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def check_solana():
-    rpc_url = "VLOZ_SVOJ_HELIUS_URL_TU"
+    rpc_url = "https://mainnet.helius-rpc.com/?api-key=3770f955-3c49-4abc-b2c6-960a7e138ee3"
     payload = {"jsonrpc": "2.0", "id": 1, "method": "getHealth"}
     response = requests.post(rpc_url, json=payload)
     return f"⚡ Status Solany: {response.json()}"
